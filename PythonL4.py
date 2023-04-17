d2042788a77360143b08ee0c63fd3fbc28040528
Dushanbe Student <dushanbe.student@UCA.corp>
2023-04-17 19:56:14 +0500

    python

--- PythonL4.py
+++ PythonL4.py
@@ -1,4 +1,32 @@
-import random
+# # Fruits =("Apple", "Cherry", "Banana")
 
-print(random.randrange(7, 999))
+# # # Ordered
+# # # Unchangeable
+
+# # print(Fruits)
+
+# MyCarInfo = {
+#     "brand":"Ford",
+#     "model":"Mondeo",
+#     "year":"2006",
+#     "millage":"1090km"
+
+# }
+
+# print(MyCarInfo["brand"])
+
+
+
+MyBiography = {
+    "Birthday":"10th March 2007",
+    "Birthplace":"Dushanbe City",
+    "Age":16,
+    "Education":"School"
+
+}
+
+for x, y in MyBiography.items():
+    print(str(x) + "  " + str(y))
+
+
 
