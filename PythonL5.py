8496b1f1a2beee46706a379d0858ec6e0b7d3c1c
Your Name <abdulbositabdulmaqsud@gmail.co>
2023-04-24 20:07:23 +0500

    New files added

---
+++ PythonL5.py
@@ -0,0 +1,20 @@
+# phone_number = "000-010-000"
+
+# for i in phone_number:
+#     if i == "-":
+#         continue
+#     print(i)
+
+
+# for i in range(1,20):
+#     if i == 11:
+#         pass 
+#     else:
+#         print(i)
+
+
+def my_function(firstNumber , secondNumber):
+    print(firstNumber + secondNumber)
+
+my_function(12, 13)
+
